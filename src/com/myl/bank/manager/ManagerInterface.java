289733c45c666e6515097429a85c1fd8cd20aada
MaoYuLu <1739938943@qq.com>
2017-07-06 09:18:08 +0800

    Revert "Signed-off-by: huagegege <18279199556@163.com>"

--- src/com/myl/bank/manager/ManagerInterface.java
+++ src/com/myl/bank/manager/ManagerInterface.java
@@ -11,52 +11,10 @@ import com.myl.bank.util.InvalidDepositException;
  *5.完成余额查询功能的实现-- inquiry 6.完成转账功能的实现--transfer 7. 完成退出系统功能的实现—exitSystem
  */
 public  interface ManagerInterface {
-	
-	/**
-	 * 完成注册功能的实现
-	 * @param s1 strin类型
-	 * @param s2
-	 */
-		void register(String s1,String s2);  
-		
-		/**
-		 * 完成登录功能的实现
-		 * @param s1
-		 * @param s2
-		 */
-		void login(String s1,String s2); 
-		
-		/**
-		 * 完成存款功能的实现
-		 * @param s1
-		 * @param s2
-		 * @param num1
-		 * @return
-		 * @throws InvalidDepositException
-		 */
-		String depositMoney(String s1, String s2,double num1) throws InvalidDepositException;    
-		
-		/**
-		 * 完成取款功能的实现
-		 * @param s1
-		 * @param s2
-		 * @param num1
-		 * @return
-		 * @throws AccountOverDrawnException
-		 */
-		String withdrawals(String s1, String s2, double num1) throws AccountOverDrawnException;  
-		
-		/**
-		 * 完成余额查询功能的实现
-		 * @param s1
-		 * @param s2
-		 * @return
-		 */
-		double inquiry(String s1, String s2); 
-		
-		/**
-		 *  转账的方法
-		 * @param s1
-		 */
-		void transfer(String s1); 
+		void register(String s1,String s2);   //完成注册功能的实现
+		void login(String s1,String s2);   //完成登录功能的实现
+		String depositMoney(String s1, String s2,double num1) throws InvalidDepositException;    //完成存款功能的实现
+		String withdrawals(String s1, String s2, double num1) throws AccountOverDrawnException;  //完成取款功能的实现
+		double inquiry(String s1, String s2); //完成余额查询功能的实现
+		void transfer(String s1); // 转账的方法
 }
